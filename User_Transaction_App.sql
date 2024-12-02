5099c357b96f0615b05edc53e8459176d941d67c
Anggiputrif <89929301+Anggiputrif@users.noreply.github.com>
2024-12-02 13:08:56 +0700

    Update User_Transaction_App.sql

--- User_Transaction_App.sql
+++ User_Transaction_App.sql
@@ -1,4 +1,6 @@
---- N0.1
+--- User yang baru bertransaksi pada bulan Februari dan total transaksinya telah mencapai 50.000 di
+bulan tersebut.
+  
 SELECT t.member_id, m.created_at AS register_at, MIN(t.created_at) AS transaksi_at
 FROM members m INNER JOIN transaksi t
 ON m.id= t.member_id
@@ -8,7 +10,8 @@ HAVING MIN(t.created_at) BETWEEN '2023-02-01 00:00:00' AND '2023-03-01 00:00:00'
 AND SUM(t.price) >= 50000
 ORDER BY MIN(t.created_at) ASC;
 
---- NO.2
+--- Persentase total user yang melakukan transaksi kembali di bulan Februari, yang mana
+user-user tersebut telah melakukan tranaksi di bulan Januari
 
 WITH PREV_DATA AS (
 SELECT COUNT(DISTINCT member_id) AS PREV
@@ -25,7 +28,7 @@ WHERE created_at BETWEEN '2023-01-01 00:00:00' AND '2023-02-01 00:00:00'
 AND status_pengisian = 'Berhasil' AND type_transaksi = 'Transaksi')
 AND status_pengisian = 'Berhasil' AND type_transaksi = 'Transaksi';
 
---- NO.3
+--- Total user per tier di bulan Januari dan Februari.
 SELECT DATE_FORMAT(t.created_at, '%M') AS bulan, mt.tier_name AS tier, COUNT(DISTINCT mt.member_id) AS total
 FROM transaksi t INNER JOIN members_tier mt
 ON t.member_id = mt.member_id
@@ -33,7 +36,7 @@ WHERE t.created_at BETWEEN '2023-01-01 00:00:00' and  '2023-03-01 00:00:00'
 GROUP BY 1,2
 ORDER BY 1,2;
 
---- NO.4
+--- Persentase pertumbuhan total transaksi tiap bulannya dari bulan Oktober 2022 – Februari 2023
 SELECT DATE_FORMAT(created_at, '%Y-%m') AS bulan, COUNT(id) AS total, 
 IFNULL(CONCAT(ROUND(100*(COUNT(id) - LAG(COUNT(id), 1, 0)
 OVER (ORDER BY DATE_FORMAT(created_at, '%Y-%m'))) / LAG(COUNT(id), 1, 0)
@@ -42,7 +45,4 @@ FROM transaksi
 WHERE type_transaksi = 'Transaksi' AND status_pengisian = 'Berhasil'
 AND created_at BETWEEN '2022-10-01 00:00:00' and '2023-03-01 00:00:00'
 GROUP BY 1;
-
---- NO.5
-
 
